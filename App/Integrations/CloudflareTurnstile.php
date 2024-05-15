cf80b13e78edec3c7f1ec5518416ed03a5ca0299
James <james@armitage.contact>
2024-05-15 18:01:24 +0930

    "PHP types and formatting for frontend"

--- App/Integrations/CloudflareTurnstile.php
+++ App/Integrations/CloudflareTurnstile.php
@@ -6,18 +6,24 @@ use FormFlow\Core\HookEventsInterface;
 use FormFlow\Data\Integration;
 
 class CloudflareTurnstile implements HookEventsInterface {
-
-    public function hook_events() {
-        $integration = new Integration( 'couldflare-turnstile' );
+    /**
+     * Intitialisation function that can be safely called to load functionalty.
+     *
+     * @return void
+     */
+    public function hook_events(): void {
+        $integration = new Integration('couldflare-turnstile');
         $integration->title = 'Cloudflare Turnstile';
         $integration->image = FORMFLOW_PLUGIN_URI . '/assets/images/Cloudflare.png';
         $integration->website = 'https://www.cloudflare.com/products/turnstile/';
         $integration->developer = 'Form Flow';
-        $integration->on_load = [ $this, 'on_load' ];
+        $integration->on_load = [$this, 'on_load'];
     }
 
-    public function on_load() {
-
-    }
-
+    /**
+     * Registers the class's validation function on FormFlow validate hook.
+     *
+     * @return void
+     */
+    public function on_load(): void {}
 }
